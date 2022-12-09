8ef48c8bf0a894385a172badf6a992e198936793
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-12-09 16:15:32 -0300

    Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
+import { Chat } from 'phosphor-react'
+import * as Popover from '@radix-ui/react-popover';
+import { WidgetForm } from '../components/WidgetForm'
 
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div className="h-screen w-screen bg-black flex flex-col gap-4 items-end justify-end p-8">
+      <h1 className="text-4xl text-white font-bold"></h1>
+
+      <Popover.Root>
+        <Popover.Portal>
+          <Popover.Content align="end" sideOffset={16}>
+            <WidgetForm />
+          </Popover.Content>
+        </Popover.Portal>
+
+        <Popover.Trigger className="bg-violet-500 rounded-full h-12 w-12 flex items-center justify-center hover:bg-violet-600">
+          <Chat className="text-white h-5 w-5" weight="bold" />
+        </Popover.Trigger>
+      </Popover.Root>
     </div>
   )
 }
